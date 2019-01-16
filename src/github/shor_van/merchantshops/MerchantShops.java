251286db2b4613b12f858b93ae6157441ec8a275
Shor-van <shorxvan01@gmail.com>
2019-01-16 13:11:59 +0100

    Fixed mistake when saving entity type

--- src/github/shor_van/merchantshops/MerchantShops.java
+++ src/github/shor_van/merchantshops/MerchantShops.java
@@ -203,7 +203,7 @@ public class MerchantShops extends JavaPlugin
     		
             //entity type
             if(merchant.getEntityType() != EntityType.VILLAGER);
-                merchantSection.set("entity-type", merchant.getEntityType());
+                merchantSection.set("entity-type", merchant.getEntityType().name().toLowerCase());
             
             //Name
             merchantSection.set("name", merchant.getMerchantEntity().getCustomName());
